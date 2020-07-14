db64b137ffc49f5b487fa7aa4df86c09684c4a68
Nickson Kaigi <nickaigi@gmail.com>
2020-07-14 17:41:40 +0300

    added views and urls

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 
-from .views import index
+from .views import index, detail, results, vote
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:question_id>/results/', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
