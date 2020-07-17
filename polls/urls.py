5d060f6f139847a3e3285ef08a92a1c5376d15ad
Nickson Kaigi <nickaigi@gmail.com>
2020-07-17 19:00:12 +0300

    url namespacing

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from .views import index, detail, results, vote
 
-
+app_name = 'polls'
 urlpatterns = [
     path('', index, name='index'),
     path('<int:question_id>/', detail, name='detail'),
